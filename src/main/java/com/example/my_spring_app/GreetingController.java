d03231f22c3669e639315a71e717216ba5c202e0
Mitch Munoz <mitchell.munoz127@gmail.com>
2025-09-18 04:17:12 +0000

    Add CORS support to allow cross-origin requests

--- src/main/java/com/example/my_spring_app/GreetingController.java
+++ src/main/java/com/example/my_spring_app/GreetingController.java
@@ -2,10 +2,12 @@ package com.example.my_spring_app;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class GreetingController {
 
